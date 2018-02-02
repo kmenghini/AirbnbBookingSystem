ef00beba946861592e9805ef53339b4a64fa70c1
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-02-01 19:16:54 -0800

    add db function to get details for one listing

---
+++ InventoryService/db/index.js
@@ -0,0 +1,19 @@
+var cassandra = require('cassandra-driver');
+
+var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'airbnb'});
+client.connect(function(err, result) {
+    console.log('Cassandra Connected')
+});
+
+module.exports = {
+  getListingDetails: (listingId, callback) => {
+    client.execute('SELECT * FROM listings WHERE id = ?;',[listingId], (err, res) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(res.rows);
+      }
+    });
+  }
+}
+
