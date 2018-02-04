fb751e154385c7bd271408075464bf1bb51fa1e2
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-02-03 16:09:18 -0800

    function to load generated bookings into postgres

---
+++ InventoryService/db/loadBookingsToPostgres.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+var csv = require('csv');
+var server = require('../app.js');
+
+module.exports.loadBookings = function(n) {
+  console.log('loading bookings for file ' + n)
+  var readStream = fs.createReadStream(`/Users/kmenghini/AirbnbData/bookings${n}copy.txt`);
+
+  var opt  = {delimiter: ','};
+
+  var parser = csv.parse(opt);
+
+  let loader = csv.transform((data, index) => {
+    var bookingObj = {
+      book_time: data[0],
+      listing_id: data[1]
+    };
+    server.processBooking(bookingObj);
+  });
+
+  readStream.pipe(parser).pipe(loader)
+    .on('finish', () => {console.log('reached end of bookings file');});
+  
+}
