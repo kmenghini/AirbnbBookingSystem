bb21b04e776eb6026ca99a7c358b9adb1689a8e7
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-02-08 12:18:25 -0800

    modify functions to account for change in sqs message format

--- InventoryService/sqs/sqsConsumer.js
+++ InventoryService/sqs/sqsConsumer.js
@@ -1,6 +1,6 @@
 const Consumer = require('sqs-consumer');
 const AWS = require('aws-sdk');
-const server = require('../app.js');
+var func = require('../helperFunctions.js');
 var awsKey = require('../config/aws.config.js');
 
 AWS.config.update({
@@ -11,11 +11,10 @@ AWS.config.update({
 
 const app = Consumer.create({
   queueUrl: 'https://sqs.us-west-1.amazonaws.com/462015734403/fromBookings',
+  messageAttributeNames: ['book_time', 'listing_id'],
   handleMessage: (message, done) => {
     console.log('got message')
-    var booking = JSON.parse(message.Body);
-    console.log(booking);
-    server.receiveBookings(booking);
+    func.receiveBookings(message.MessageAttributes.book_time.StringValue, message.MessageAttributes.listing_id.StringValue);
     done();
   },
   sqs: new AWS.SQS()
